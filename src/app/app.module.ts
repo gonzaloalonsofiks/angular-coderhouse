ae96c9fae1405dc3d6749aa1e9e57557666dafbc
gonzaloalonsofiks <gonzaloalonsofiks@gmail.com>
2023-10-02 22:58:16 -0300

    Add userForm to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +18,11 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
     SidebarComponent,
     FooterComponent,
     MainComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    UserFormComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
