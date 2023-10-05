8632ecb187745e8bc8bba05615db934cd32ab3b8
gonzaloalonsofiks <gonzaloalonsofiks@gmail.com>
2023-10-05 08:41:43 -0300

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { UserFormComponent } from './user-form/user-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { UserFormComponent } from './user-form/user-form.component';
     EmployeeListComponent,
     UserFormComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
