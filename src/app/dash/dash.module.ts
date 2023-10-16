b79157f4364a0970eee7fd0b1bba39cd5406c296
plafagency <gonzalo@plaf.agency>
2023-10-16 17:06:23 -0300

    Update Dashboard

--- src/app/dash/dash.module.ts
+++ src/app/dash/dash.module.ts
@@ -2,25 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashComponent } from './dash.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { HomeModule } from './pages/home/home.module';
-import { FormsModule } from './pages/forms/forms.module';
+import { FormsModule } from './pages/forms/new-user.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
 import { UsersModule } from './pages/users/users.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [DashComponent],
   imports: [
     CommonModule,
+    SharedModule,
     MatSidenavModule,
-    MatButtonModule,
     HomeModule,
-    MatIconModule,
     FormsModule,
     MatToolbarModule,
-    MatListModule,
     UsersModule,
   ],
   exports: [DashComponent],
