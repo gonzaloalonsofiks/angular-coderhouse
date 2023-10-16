01751f9acb166cbde0affe1e1fc1060ebd198d7d
plafagency <gonzalo@plaf.agency>
2023-10-16 11:58:32 -0300

    Add Dash component

---
+++ src/app/dash/dash.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dash',
+  templateUrl: './dash.component.html',
+  styleUrls: ['./dash.component.scss'],
+})
+export class DashComponent {
+  showFiller = false;
+
+  menu = ['Inicio', 'Usuarios', 'Aprendizaje'];
+}
